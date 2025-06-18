aa431313d4a30d9652a7d680e326287b56f72255
miroslavVakulenko <vakulmir@gmail.com>
2025-06-18 14:28:18 +0300

    cards arr

--- blocks/lazyblock-personalities--cards/block.php
+++ blocks/lazyblock-personalities--cards/block.php
@@ -1,17 +1,22 @@
 <?php
-$cards = $attributes['cards']  ?? [];
-$photo = $attributes['photo']['url'] ?? '';
-$name = $attributes['name'] ?? '';
-$role = $attributes['role'] ?? '';
+$cards = $attributes['cards'] ?? [];
 ?>
-
 
 <div class="lazyblock-personalities-cards">
   <div class="personalities-cards-container">
-    <div class="personalities-card">
-      <img src="<?php echo esc_url($photo); ?>" alt="<?php echo esc_attr($name); ?>" />
-      <h3><?php echo esc_html($name); ?></h3>
-      <p><?php echo esc_html($role); ?></p>
-    </div>
+    <?php if (!empty($cards) && is_array($cards)) : ?>
+      <?php foreach ($cards as $card) : ?>
+        <?php
+        $photo = $card['photo']['url'] ?? '';
+        $name = $card['name'] ?? '';
+        $role = $card['role'] ?? '';
+        ?>
+        <div class="personalities-card">
+          <img src="<?php echo esc_url($photo); ?>" alt="<?php echo esc_attr($name); ?>" />
+          <h3><?php echo esc_html($name); ?></h3>
+          <p><?php echo esc_html($role); ?></p>
+        </div>
+      <?php endforeach; ?>
+    <?php endif; ?>
   </div>
 </div>  
